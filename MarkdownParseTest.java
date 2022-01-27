8a624470f98daaf3fb89c418ca9eb9f644a6e958
Ryan Kan <kanryan@gmail.com>
2022-01-26 20:39:11 -0800

    error fix

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -3,20 +3,30 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.io.IOException;
 import org.junit.*;
-import java.util.ArrayList;
+import java.util.List;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
-
+    
     @Test
-    public void testGetLinks() throws IOException{
-        ArrayList<String> list = List.of("https://something.com", "some-page.html");
+    public void testParse() throws IOException{
         Path fileName = Path.of("test-file.md");
 	    String contents = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(contents);
-        assertEquals(list, links);
+        assertEquals(List.of("https://something.com","some-page.html"), MarkdownParse.getLinks(contents));
+    }
+    @Test
+    public void testParse1() throws IOException{
+        Path fileName = Path.of("test2.md");
+	    String contents = Files.readString(fileName);
+        assertEquals(List.of(""), MarkdownParse.getLinks(contents));
+    }
+    @Test
+    public void testParse2() throws IOException{
+        Path fileName = Path.of("test3.md");
+	    String contents = Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(contents));
     }
 }
