a847b4af9344a029ce5152cb685494907127042d
Ryan Kan <kanryan@gmail.com>
2022-02-24 17:03:04 -0800

    Lab4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -4,6 +4,7 @@ import java.nio.file.Path;
 import java.io.IOException;
 import org.junit.*;
 import java.util.List;
+import java.util.ArrayList;
 
 public class MarkdownParseTest {
     @Test
@@ -15,18 +16,45 @@ public class MarkdownParseTest {
     public void testParse() throws IOException{
         Path fileName = Path.of("test-file.md");
 	    String contents = Files.readString(fileName);
-        assertEquals(List.of("https://something.com","some-page.html"), MarkdownParse.getLinks(contents));
+        assertEquals(List.of("image.png","some-page.html"), MarkdownParse.getLinks(contents));
     }
     @Test
     public void testParse1() throws IOException{
         Path fileName = Path.of("test2.md");
 	    String contents = Files.readString(fileName);
-        assertEquals(List.of(""), MarkdownParse.getLinks(contents));
+        assertEquals(List.of("image.url", "some-page.html"), MarkdownParse.getLinks(contents));
     }
     @Test
     public void testParse2() throws IOException{
         Path fileName = Path.of("test3.md");
 	    String contents = Files.readString(fileName);
         assertEquals(List.of(), MarkdownParse.getLinks(contents));
+    }
+
+    
+
+    @Test
+    public void testSnippet1() throws IOException{
+        Path fileName = Path.of("snippet1.md");
+	    String contents = Files.readString(fileName);
+        assertEquals(List.of("url.com", "google.com"), review.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet2() throws IOException{
+        Path fileName = Path.of("snippet2.md");
+	    String contents = Files.readString(fileName);
+        assertEquals(List.of("b.com"), review.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException{
+        Path fileName = Path.of("snippet3.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> list = new ArrayList<>();
+        list.add("https://www.twitter.com");
+        list.add("https://ucsd-cse15l-w22.github.io/");
+        list.add("https://cse.ucsd.edu/");
+        assertEquals(list, review.getLinks(contents));
     }
 }
